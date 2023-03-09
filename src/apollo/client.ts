c74a9aa858d88ce06ba35867a146f11e571b5835
Katarka <www.runit@gmail.com>
2023-03-09 22:20:03 +0300

    add apollo config

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,7 +1,7 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'kek/api',
+    uri: 'http://localhost:5000/api/graphql',
     cache: new InMemoryCache(),
 })
 
